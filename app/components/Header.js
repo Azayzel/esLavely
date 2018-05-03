70b9bc1ffbeca8261b707ae2fd54ebe0a376eb83
Josh Lavely <josh.lavely@gmail.com>
2018-05-02 23:14:41 -0400

    Added more secrets and header title

--- app/components/Header.js
+++ app/components/Header.js
@@ -42,12 +42,13 @@ class Header extends React.Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <IndexLink to="/" className="navbar-brand">Project name</IndexLink>
+            <IndexLink to="/" className="navbar-brand">esLavely</IndexLink>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
               <li><IndexLink to="/" activeStyle={active}>Home</IndexLink></li>
               <li><Link to="/contact" activeStyle={active}>Contact</Link></li>
+              <li><Link to="/contact" activeStyle={active}>Blog</Link></li>
             </ul>
             {rightNav}
           </div>
